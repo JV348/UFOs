6e7b00e4eb7c73670dc943f68b04f8f7d965b8f1
vil141 <j.villanueva5@utexas.edu>
2022-03-04 21:45:05 -0600

    Updating and filtering data

--- static/js/app.js
+++ static/js/app.js
@@ -6,6 +6,7 @@ var tbody = d3.select("tbody");
 
 // // Create a table with the data; clear previous table data
 function buildTable(data) {
+    // First, clear out any existing data
     tbody.html("");
     
     // Add forEach to the table; forEach function that loops through our data array, and then adds rows of data to the table
@@ -16,6 +17,33 @@ function buildTable(data) {
         Object.values(dataRow).forEach((val) => {
             let cell = row.append["td"]
             cell.text(val);
-        });
+            }
+        );
     });
 }
+
+// // Second function that will focus on filtering the table 
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+  
+     // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.datetime === date);
+    }
+  
+     // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+  }
+  
+  // Attach an event to listen for the form button
+  d3.selectAll("#filter-btn").on("click", handleClick);
+  
+  // Build the table when the page loads
+  buildTable(tableData);
